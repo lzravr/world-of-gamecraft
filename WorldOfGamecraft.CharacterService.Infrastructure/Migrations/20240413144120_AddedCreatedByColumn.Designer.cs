3c872dcb8e862389e493397a29158e321b3a070c
Lazar Avramovic <lzr.avr@gmail.com>
2024-04-13 17:06:49 +0200

    Added createdBy column for character

--- WorldOfGamecraft.CharacterService.Infrastructure/Migrations/20240413144120_AddedCreatedByColumn.Designer.cs
+++ WorldOfGamecraft.CharacterService.Infrastructure/Migrations/20240413144120_AddedCreatedByColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WorldOfGamecraft.CharacterService.Infrastructure;
@@ -11,9 +12,11 @@ using WorldOfGamecraft.CharacterService.Infrastructure;
 namespace WorldOfGamecraft.CharacterService.Infrastructure.Migrations
 {
     [DbContext(typeof(CharactersDbContext))]
-    partial class CharactersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240413144120_AddedCreatedByColumn")]
+    partial class AddedCreatedByColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
